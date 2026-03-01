737e842b2c36a1d09bebe144b48d857841e9ca9f
goyodp <goyo1988@gmail.com>
2026-03-01 15:19:01 -0600

    Create app.js

---
+++ src/firebase/app.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTKEpKl...",
+  authDomain: "golf-scorecard---gdpdv.firebaseapp.com",
+  projectId: "golf-scorecard---gdpdv",
+  storageBucket: "golf-scorecard---gdpdv.firebasestorage.app",
+  messagingSenderId: "575461986592",
+  appId: "1:575461986592:web:fcf85b6994235b5ce80745",
+  measurementId: "G-4FQQ7GHQ2T"
+};
+
+export const app = initializeApp(firebaseConfig);

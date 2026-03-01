caa76b5da7249c2352b04f834c1d51787fc969e0
goyodp <goyo1988@gmail.com>
2026-03-01 17:24:48 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
+import Session from "./screens/Session";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/session/:sessionId" element={<Session />} />
+    </Routes>
+  );
 }

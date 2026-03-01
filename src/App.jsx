2bd1f71ed1f41598b964eef14f3ad7290cd51212
goyodp <goyo1988@gmail.com>
2026-03-01 17:45:46 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./screens/Home";
 import Session from "./screens/Session";
+import GroupScorecard from "./screens/GroupScorecard";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/session/:sessionId" element={<Session />} />
+      <Route path="/session/:sessionId/group/:groupId" element={<GroupScorecard />} />
+
+      {/* fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }

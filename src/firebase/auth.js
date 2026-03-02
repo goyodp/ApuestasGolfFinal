227100c9ab38d3a61569f1b95954807383eecf33
goyodp <goyo1988@gmail.com>
2026-03-02 08:12:31 -0600

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,5 +1,8 @@
+// src/firebase/auth.js
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { app } from "./app";
 
 export const auth = getAuth(app);
+
+// Aún lo puedes dejar por si luego usas web login/redirect
 export const googleProvider = new GoogleAuthProvider();

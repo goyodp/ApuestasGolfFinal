f39e347886a702b0594469dd9263fab32b931f15
goyodp <goyo1988@gmail.com>
2026-03-02 14:17:27 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,14 @@
-import type { CapacitorConfig } from "@capacitor/cli";
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.apuestas.golf",
-  appName: "Apuestas Golf",
-  webDir: "dist",
-  bundledWebRuntime: false,
+  appId: 'com.apuestas.golf',
+  appName: 'Apuestas Golf',
+  webDir: 'dist',
+  plugins: {
+    FirebaseAuthentication: {
+      providers: ['google.com', 'apple.com'],
+    },
+  },
 };
 
 export default config;
